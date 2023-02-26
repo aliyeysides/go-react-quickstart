493b336291368bc0ec9c531b73ee665377a640c0
Ali Yeysides <ali.yeysides@gmail.com>
2023-02-26 01:10:41 -0500

    format

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
-  app.Static("/", "client/dist")
+	app.Static("/", "client/dist")
 
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
