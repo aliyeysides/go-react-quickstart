6a60e831ac70bd5d4c7030eed37b96c9a29208de
Ali Yeysides <ali.yeysides@gmail.com>
2023-02-26 01:08:21 -0500

    init commit

---
+++ server.go
@@ -0,0 +1,15 @@
+// Go react fiber server example using React
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+  app.Static("/", "client/dist")
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Listen(":3000")
+}
