95631e6a8ad8975c93744787e030d5a0a08c39b0
Ali Yeysides <ali.yeysides@gmail.com>
2023-02-26 14:20:10 -0500

    setsup tailwindcss

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
